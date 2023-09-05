957cdd2095fb2e00da35cc559f7cc756a1375150
shoooya <shoooya17@gmail.com>
2023-09-05 18:34:48 +0900

    refactor

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: process.env.VITE_PORT,
+    },
+    hmr: {
+        host: process.env.VITE_HOST ?? 'localhost',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
